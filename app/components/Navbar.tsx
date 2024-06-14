84a2d932de89da88b122e7cdaa93c038abe4d7b7
Vidya Az Zahra <92587197+vidyazh@users.noreply.github.com>
2024-06-14 14:38:26 +0800

    Add Log out Button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import { ThemeToggle } from "./Themetoggle";
 import { Button } from "@/components/ui/button";
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+export async function Navbar() {
+    const { isAuthenticated} = getKindeServerSession();
     return(
         <nav className="border-b bg-background h-[10vh] flex items-center">
             <div className="container flex items-center justify-between">
@@ -14,14 +16,16 @@ export function Navbar() {
             <div className="flex items-center gap-x-5">
                 <ThemeToggle/>
 
-                <div className="flex items-center gap-x-5">
+                {await isAuthenticated() ? (
+                    <Button>Log out</Button>
+                ) : (
+                    <div className="flex items-center gap-x-5">
                     <LoginLink><Button>Sign In</Button></LoginLink>
                     <RegisterLink><Button variant="secondary">Sign Up</Button></RegisterLink>
                 </div>
+                )}
             </div>
-            
             </div>
-
         </nav>
     )
 }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return (
+        <div>
+            <h1>hello from the dashboard</h1>
+        </div>
+    )
+}
